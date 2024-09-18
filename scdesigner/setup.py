3a2021eab61be543085195af6bd5296aecb4c345
Kris Sankaran <sankaran.kris@gmail.com>
2024-09-17 19:37:34 -0500

    add pandas dependency

--- scdesigner/setup.py
+++ scdesigner/setup.py
@@ -4,7 +4,7 @@ setup(
     name='scdesigner',
     version='0.0.999',
     packages=find_packages(),
-    install_requires=['torch', 'numpy'],
+    install_requires=['torch', 'numpy', 'pandas'],
     description='Interactive simulation for rigorous and transparent multi-omics analysis',
     author='Kris Sankaran',
     author_email='ksankaran@wisc.edu',
