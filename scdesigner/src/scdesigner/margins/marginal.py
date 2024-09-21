bcb8931a329b2b9ba1fa70d1a2c75477da02c4fe
Kris Sankaran <sankaran.kris@gmail.com>
2024-09-21 14:05:52 -0500

    update example with estimation examples

--- scdesigner/src/scdesigner/margins/marginal.py
+++ scdesigner/src/scdesigner/margins/marginal.py
@@ -13,21 +13,5 @@ class Marginal():
     def logliklihood(self, Y, X=None):
         pass
 
-class MarginalList():
-    def __init__(self, marginals):
-        super().__init__()
-        self.marginals = marginals
-
-
-    def fit(self, Y, X=None, index=None, **kwargs):
-        if index is None:
-            index = range(self.marginals)
-        for ix in index:
-            self.margins[ix].fit(X, Y, **kwargs)
-
-    
-    def sample(self, Y, X=None, **kargs):
-        pass
-
-    def logliklihood(self, Y, X=None):
+    def quantile(self, Y, q):
         pass
