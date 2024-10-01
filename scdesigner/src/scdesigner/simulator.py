398d225146c09c090cb1ded2218e397a28f50568
Kris Sankaran <sankaran.kris@gmail.com>
2024-10-01 18:29:50 -0500

    `margin_apply` abstracts the code used by both reformulate and nullify

--- scdesigner/src/scdesigner/simulator.py
+++ scdesigner/src/scdesigner/simulator.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from .margins.marginal import args
-from .margins.reformulate import reformulate, match_marginal
+from .margins.reformulate import reformulate, match_marginal, nullify_formula
 from torch.optim import LBFGS
 from torch.utils.data import DataLoader
 import pandas as pd
@@ -21,22 +21,21 @@ class Simulator:
         super().__init__()
         self.margins = margins
         self.copula = copula
+        self.anndata = None
 
     def fit(self, anndata, max_epochs=10):
         for margin in self.margins:
             y_names, submodel = margin
             submodel.fit(anndata[:, y_names], max_epochs)
+        self.anndata = anndata
 
-    def reformulate(self, genes, formula, anndata, max_epochs=10):
-        ix, matched = match_marginal(self.margins, genes)
-        for i in sorted(ix, reverse=True):
-            del self.margins[i]
+    def reformulate(self, genes, formula, max_epochs=10):
+        def f(margin, genes):
+            return reformulate(margin, genes, formula, self.anndata)
 
-        for _, margin in matched:
-            new, unchanged = reformulate(margin, genes, formula, anndata)
-            g1, g2 = (m.module.gene_names for m in (new, unchanged))
-            new.fit(anndata[:, g1], max_epochs)
-            self.margins += [(g1, new), (g2, unchanged)]
+        self.margins = margin_apply(
+            self.margins, genes, f, self.anndata, max_epochs=max_epochs
+        )
 
     def predict(self, obs):
         param_hat = []
@@ -44,10 +43,31 @@ class Simulator:
             param_hat.append([genes, margin.predict(obs)])
         return merge_predictions(param_hat)
 
+    def nullify(self, term, genes, max_epochs=10):
+        def f(margin, genes):
+            null_formula = nullify_formula(margin.formula, term)
+            return reformulate(margin, genes, null_formula, self.anndata)
+
+        self.margins = margin_apply(
+            self.margins, genes, f, self.anndata, max_epochs=max_epochs
+        )
 
 def safe_update(d1, d2):
     d1.update({k: v for k, v in d2.items() if k not in d1})
     return d1
+
+def margin_apply(margins, genes, f, anndata, **kwargs):
+    ix, matched = match_marginal(margins, genes)
+    for i in sorted(ix, reverse=True):
+        del margins[i]
+
+    for _, margin in matched:
+        new, unchanged = f(margin, genes)
+        g1, g2 = (m.module.gene_names for m in (new, unchanged))
+        new.fit(anndata[:, g1], **kwargs)
+        margins += [(g1, new), (g2, unchanged)]
+    return margins
+
 
 
 def simulator(anndata, margins, delay=False, copula=None, max_epochs=10, **kwargs):
