edfeb5041f436d6e1917b5d84b8518ae797e23d7
Kris Sankaran <sankaran.kris@gmail.com>
2024-10-06 16:08:21 -0500

    support for pamona join

--- scdesigner/src/scdesigner/join.py
+++ scdesigner/src/scdesigner/join.py
@@ -1,30 +1,38 @@
 import anndata as ad
-import numpy as np
 import muon as mu
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+from pamona import Pamona
 from copulas.multivariate import GaussianMultivariate
 from scdesigner import simulator as sim
 from .copula import ScCopula
         
-def update_margins(sim, i="1"):
+def update_margin_varnames(sim, i="1"):
     new_margins = []
     for genes, margin in sim.margins:
         new_genes = [f"source{i}_{g}" for g in genes]
         new_margins.append((new_genes, margin))
     return new_margins
 
-def join_copula(sim1, sim2, copula_type=GaussianMultivariate, cov_fun=np.cov):
-    a1, a2 = sim1.anndata, sim2.anndata
+def reconcile_names(adata, sims):
+    if not (set(adata["s1"].var_names) & set(adata["s2"].var_names)):
+        return adata, sims
 
-    # reconcile variable names if there is overlap
-    if set(a1.var_names) & set(a2.var_names):
-        a1.var_names = [f"source1_{a}" for a in a1.var_names]
-        a2.var_names = [f"source2_{a}" for a in a2.var_names]
-        sim1.margins = update_margins(sim1)
-        sim2.margins = update_margins(sim2, "2")
+    new_adata, new_sims = deepcopy(adata), deepcopy(sims)
+    for k, v in adata.items():
+        new_adata[k].var_names = [f"source{k}_{a}" for a in v.var_names]
+        new_sims[k].margins = update_margin_varnames(new_sims[k], k)
+    return new_adata, new_sims
+
+def join_copula(sim1, sim2, copula_type=GaussianMultivariate, cov_fun=np.cov):
+    sims = {"s1": sim1, "s2": sim2}
+    adata = {"s1": sim1.anndata, "s2": sim2.anndata}
+    adata, sims = reconcile_names(adata, sims)
 
     # combine features
-    adata = ad.concat([a1, a2], axis=1, merge="same", label="modality_")
-    margins = list(sim1.margins + sim2.margins)
+    adata = ad.concat(adata.values(), axis=1, merge="same", label="modality_")
+    margins = list(sims["s1"].margins + sims["s2"].margins)
     joined = sim.Simulator(margins, ScCopula(copula_type, cov_fun))
 
     # refit the copula
@@ -32,6 +40,39 @@ def join_copula(sim1, sim2, copula_type=GaussianMultivariate, cov_fun=np.cov):
     joined.anndata = adata
     joined.anndata.var 
     return joined
+
+def join_pamona(sim1, sim2, multivariate=None, priority="mu", pamona_opts={}, **kwargs):
+    sims = {"s1": sim1, "s2": sim2}
+    adata = {"s1": sim1.anndata, "s2": sim2.anndata}
+    adata, sims = reconcile_names(adata, sims)
+    Pa = Pamona.Pamona(**pamona_opts)
+    embeddings, _ = Pa.run_Pamona([adata["s1"].X, adata["s2"].X])
+
+    # add the learned embedding information to the anndata
+    dims = [f"pamona_{i}" for i in range(embeddings[0].shape[1])]
+    embeddings = {
+        "s1": pd.DataFrame(embeddings[0], columns=dims),
+        "s2": pd.DataFrame(embeddings[1], columns=dims)
+    }
+
+    margins = []
+    for k in adata.keys():
+        obs = adata[k].obs.reset_index(drop=True)
+        adata[k].obs = pd.concat([obs, embeddings[k]], axis=1)
+        for genes, margin in sims[k].margins:
+            new_margin = deepcopy(margin)
+            if isinstance(margin.formula, str):
+                new_margin.formula = f"{margin.formula} + {' + '.join(dims)}"
+            else:
+                new_margin.formula[priority] = f"{margin.formula[k]} + {' + '.join(dims)}"
+            margins.append((genes, new_margin))
+
+    # define and fit the simulator
+    joined = sim.Simulator(margins, multivariate)
+    adata = ad.concat(adata.values(), join="outer", label="modality_", axis=0)
+    joined.fit(adata, **kwargs)
+    return joined
+    
 
 def split_adata(adata):
     var_names = adata.var.groupby("modality_")\
