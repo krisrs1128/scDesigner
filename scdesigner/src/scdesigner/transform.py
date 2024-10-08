9d8418c5b67ce87e2dcc479a4bc89df6dfa6a986
Kris Sankaran <sankaran.kris@gmail.com>
2024-10-08 15:35:19 -0500

    Function for amplifying/dampening parameters, for power analysis

---
+++ scdesigner/src/scdesigner/transform.py
@@ -0,0 +1,33 @@
+from copy import deepcopy
+
+
+def amplify(simulator, factor, parameter, genes):
+    sim = deepcopy(simulator)
+    transform_parameter(lambda x: factor * x, sim, parameter, genes)
+    return sim
+
+
+def dampen(simulator, factor, parameter, genes):
+    sim = deepcopy(simulator)
+    transform_parameter(lambda x: x / factor, sim, parameter, genes)
+    return sim
+
+
+def substring_match(feature, alter_features):
+    for f in alter_features:
+        if f in feature:
+            return True
+    return False
+
+
+def transform_parameter(f, simulator, alter_features, alter_genes=None, parameter="mu"):
+    for genes, margin in simulator.margins:
+        if alter_genes is None:
+            alter_genes = genes
+
+        for theta in margin.module.linear[parameter].parameters():
+            features = margin.configure_loader(simulator.anndata).dataset.features[parameter]
+            gene_ix = [i for i, v in enumerate(genes) if v in alter_genes]
+
+            feature_ix = [i for i, v in enumerate(features) if substring_match(v, alter_features)]
+            theta.data[feature_ix, gene_ix] = f(theta[feature_ix, gene_ix])
