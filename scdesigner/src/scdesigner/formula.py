690f14b483606364825436cbbe3ce46558a43c87
Kris Sankaran <sankaran.kris@gmail.com>
2024-10-10 22:03:25 -0500

    define dataset classes for on disk vs. in memory

--- scdesigner/src/scdesigner/formula.py
+++ scdesigner/src/scdesigner/formula.py
@@ -21,57 +21,63 @@ def initialize_formula(f, parameters=["alpha", "mu"], priority="mu"):
 class FormulaDataset(Dataset):
     def __init__(self, formula, adata, **kwargs):
         if adata.isbacked:
-            return FormulaDatasetOnDisk(formula, adata, **kwargs)
+            ds = FormulaDatasetOnDisk(formula, adata, **kwargs)
         else:
-            return FormulaDatasetInMemory(formula, adata, **kwargs)
+            ds = FormulaDatasetInMemory(formula, adata, **kwargs)
 
-class FormulaDatasetInMemory(Dataset):
+        self.__class__ = ds.__class__
+        self.__dict__ = ds.__dict__
+
+class _FormulaDataset(Dataset):
     def __init__(self, formula, adata, **kwargs):
         self.len = len(adata)
+        self.formula = initialize_formula(formula, **kwargs)
+        self.obs = dict.fromkeys(self.formula.keys(), [])
+        self.features = dict.fromkeys(self.formula.keys(), [])
+        self.adata = adata
+        
+        for k, f in self.formula.items():
+            self.formula[k] = parse_formula(f, adata.obs.columns)
+            model_df = model_matrix(self.formula[k], adata.obs.copy())
+            self.features[k] = list(model_df.columns)
+            self.obs[k] = torch.from_numpy(np.array(model_df).astype(np.float32))
+
+    def __len__(self):
+        return self.len
+
+class FormulaDatasetInMemory(_FormulaDataset):
+    def __init__(self, formula, adata, **kwargs):
+        super().__init__(formula, adata, **kwargs)
+        if "csc" in str(type(adata.X)):
+            X = self.adata.X.toarray()
+        else:
+            X = self.adata.X
+
         self.X = (
-            torch.from_numpy(adata.X)
+            torch.from_numpy(X.astype(np.float32))
             if adata.X is not None
             else torch.tensor([[float("nan")] * len(adata)]).reshape(-1, 1)
         )
 
-        self.formula = initialize_formula(formula, **kwargs)
-        self.obs = dict.fromkeys(self.formula.keys(), [])
-        self.features = dict.fromkeys(self.formula.keys(), [])
-        
-        for k, f in self.formula.items():
-            self.formula[k] = parse_formula(f, adata.obs.columns)
-            model_df = model_matrix(self.formula[k], adata.obs.copy())
-            self.features[k] = list(model_df.columns)
-            self.obs[k] = torch.from_numpy(np.array(model_df).astype(np.float32))
-
-    def __len__(self):
-        return self.len
-
     def __getitem__(self, ix):
         return self.X[ix], {k: self.obs[k][ix] for k in self.obs.keys()}
 
-class FormulaDatasetOnDisk(Dataset):
+class FormulaDatasetOnDisk(_FormulaDataset):
     def __init__(self, formula, adata, **kwargs):
-        self.len = len(adata)
-        self.X = adata.X
-        self.formula = initialize_formula(formula, **kwargs)
-        self.obs = dict.fromkeys(self.formula.keys(), [])
-        self.features = dict.fromkeys(self.formula.keys(), [])
-        
-        for k, f in self.formula.items():
-            self.formula[k] = parse_formula(f, adata.obs.columns)
-            model_df = model_matrix(self.formula[k], adata.obs.copy())
-            self.features[k] = list(model_df.columns)
-            self.obs[k] = torch.from_numpy(np.array(model_df).astype(np.float32))
+        super().__init__(formula, adata, **kwargs)
+        self.is_sparse = "csc" in str(type(adata.X[0, 0]))
 
-    def __len__(self):
-        return self.len
 
     def __getitem__(self, ix):
+        if self.is_sparse:
+            X = self.adata.X[ix].toarray()
+        else:
+            X = self.adata.X[ix]
+
         X = (
-            torch.from_numpy(self.X[ix])
-            if self.X is not None
-            else torch.tensor([[float("nan")]]).reshape(1, -1)#torch.tensor([[float("nan")] * len(ix)]).reshape(-1, 1)
+            torch.from_numpy(X.astype(np.float32))
+            if self.adata.X is not None
+            else torch.tensor([[float("nan")]]).reshape(1, -1)
         )
 
         return X, {k: self.obs[k][ix] for k in self.obs.keys()}
