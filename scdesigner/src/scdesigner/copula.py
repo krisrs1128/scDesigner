33a4d24906620392722f97fc49408f1b6ddb28b9
Kris Sankaran <sankaran.kris@gmail.com>
2024-10-24 19:12:11 -0500

    externalize copula functions, to prepare for formula versions

--- scdesigner/src/scdesigner/copula.py
+++ scdesigner/src/scdesigner/copula.py
@@ -10,6 +10,25 @@ def cov2cor(c):
     D = 1 / np.sqrt(np.diag(c))
     return D * c * D
 
+def margins_invert(margins, u, obs):
+    var_names, counts = [], []
+    for genes, margin in margins:
+        var_names += list(genes)
+        u_ = torch.from_numpy(np.array(u[genes]))
+        counts.append(margin.icdf(u_, obs).numpy())
+
+    return var_names, counts
+
+def margins_uniformize(margins, anndata):
+    u, var_names = [], []
+    for genes, margin in margins:
+        var_names += list(genes)
+        X = torch.from_numpy(anndata[:, genes].X)
+        u_ = margin.cdf(X, anndata.obs).numpy()
+        u.append(np.clip(u_, 0.0001, 0.9999)) # avoid nan in inverse quantiles
+    u = pd.DataFrame(np.concatenate(u, axis=1), columns=var_names)
+    return u
+
 class ScCopula:
     def __init__(self, copula_type=GaussianMultivariate, cov_fun=np.cov):
         self.copula = copula_type(distribution=UniformUnivariate)
@@ -17,25 +36,11 @@ class ScCopula:
 
     def sample(self, margins, obs):
         u = self.copula.sample(len(obs))
-        var_names, counts = [], []
-        for genes, margin in margins:
-            var_names += list(genes)
-            u_ = torch.from_numpy(np.array(u[genes]))
-            counts.append(margin.icdf(u_, obs).numpy())
-
-        return var_names, counts
+        return margins_invert(margins, u, obs)
 
     def fit(self, margins, anndata):
-        # define the uniform data for estimation
-        u, var_names = [], []
-        for genes, margin in margins:
-            var_names += list(genes)
-            X = torch.from_numpy(anndata[:, genes].X)
-            u_ = margin.cdf(X, anndata.obs).numpy()
-            u.append(np.clip(u_, 0.0001, 0.9999)) # avoid nan in inverse quantiles
-
         # fit the copula
-        u = pd.DataFrame(np.concatenate(u, axis=1), columns=var_names)
+        u = margins_uniformize(margins, anndata)
         self.copula.fit(u)
 
         # apply custom covariance estimator
