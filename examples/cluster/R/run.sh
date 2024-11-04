0df7c4c124fd6630bff4083f60bf3b9d3633137b
Kris Sankaran <sankaran.kris@gmail.com>
2024-11-04 09:58:55 -0600

    debugging docker error

--- examples/cluster/R/run.sh
+++ examples/cluster/R/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# condor_submit -i requirements.submit
+
 # download relevant data and code
 wget https://drive.google.com/uc?export=download&id=1dhgi6oEQmyex_nRfFYUkRY0c2q0E3BgP
 git clone git@github.com:krisrs1128/scDesigner.git
