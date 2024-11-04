56d1a21dd6770bf90e1adc66e42f19ea86af857d
Kris Sankaran <sankaran.kris@gmail.com>
2024-11-04 09:43:30 -0600

    organizing the cluster runs

---
+++ examples/cluster/R/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# download relevant data and code
+wget https://drive.google.com/uc?export=download&id=1dhgi6oEQmyex_nRfFYUkRY0c2q0E3BgP
+git clone git@github.com:krisrs1128/scDesigner.git
+
+# run the script
+cd scDesigner/examples/
+Rscript -e "rmarkdown::render('scalability_study.Rmd', params = list(config=1))"
+
+# collect the data
+cp *.csv ../../
