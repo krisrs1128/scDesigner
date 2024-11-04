9d67cccc831a9b38f002268842e4c7a51f4eb2eb
Kris Sankaran <sankaran.kris@gmail.com>
2024-11-04 11:02:38 -0600

    add dependencies to docker

--- examples/cluster/R/run.sh
+++ examples/cluster/R/run.sh
@@ -4,7 +4,7 @@
 
 # download relevant data and code
 wget -O scalability_configurations.csv "https://drive.google.com/uc?export=download&id=1dhgi6oEQmyex_nRfFYUkRY0c2q0E3BgP"
-wget -O million_cells.tar.gz https://uwmadison.box.com/shared/static/fooedjcbf1roafwuy9kmr810rfvxztlc.gz
+wget -O million_cells.tar.gz "https://uwmadison.box.com/shared/static/fooedjcbf1roafwuy9kmr810rfvxztlc.gz"
 git clone https://krisrs1128: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@github.com/krisrs1128/scDesigner.git
 
 # reorganize and extract the data
--- examples/cluster/python/run.sh
+++ examples/cluster/python/run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+# download relevant data and code
+curl -L scalability_configurations.csv "https://drive.google.com/uc?export=download&id=1dhgi6oEQmyex_nRfFYUkRY0c2q0E3BgP"
+curl -L million_cells.h5ad "https://uwmadison.box.com/shared/static/9ucgdbgbt119tx1b9y3v9u7wjm9ef920.h5ad"
+git clone https://krisrs1128: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@github.com/krisrs1128/scDesigner.git
