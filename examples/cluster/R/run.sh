be93fd4a3522052183f03dcc014c800e361d8829
Kris Sankaran <sankaran.kris@gmail.com>
2024-11-04 10:49:50 -0600

    revising R script defn

--- examples/cluster/R/run.sh
+++ examples/cluster/R/run.sh
@@ -3,12 +3,12 @@
 # condor_submit -i requirements.submit
 
 # download relevant data and code
-wget https://drive.google.com/uc?export=download&id=1dhgi6oEQmyex_nRfFYUkRY0c2q0E3BgP
-wget https://drive.google.com/uc?export=download&id=1xLB9NMT859kpz-iTzkNlPDL32dDPyLOx
-wget https://drive.google.com/uc?export=download&id=1VBJ3lcUbG-1V-2o_saRgW4tgmpPPkdbO
-git clone git@github.com:krisrs1128/scDesigner.git
+wget -O scalability_configurations.csv "https://drive.google.com/uc?export=download&id=1dhgi6oEQmyex_nRfFYUkRY0c2q0E3BgP"
+wget -O million_cells.tar.gz https://uwmadison.box.com/shared/static/fooedjcbf1roafwuy9kmr810rfvxztlc.gz
+git clone https://krisrs1128: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@github.com/krisrs1128/scDesigner.git
 
 # reorganize and extract the data
+mkdir scDesigner/examples/data
 mv scalability_configurations.csv scDesigner/examples/data/
 mv million_cells* scDesigner/examples/data/
 
@@ -17,7 +17,7 @@ tar -zxvf million_cells.tar.gz
 
 # run the script
 cd ../
-Rscript -e "rmarkdown::render('scalability_study.Rmd', params = list(config=${process}))"
+Rscript -e "rmarkdown::render('scalability_study.Rmd', params = list(config=${Process}))"
 
 # collect the data
-cp *.csv $CONDOR_RUN_DIR
+cp *.csv $CONDOR_SCRATCH_DIR
