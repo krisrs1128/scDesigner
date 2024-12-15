9ed289e0d3bba222eaec8913f7ed925890b78b3f
Kris Sankaran <sankaran.kris@gmail.com>
2024-12-15 14:40:24 -0800

    make sure batch process is reusable

--- scdesigner/src/scdesigner/experimental/samplers.py
+++ scdesigner/src/scdesigner/experimental/samplers.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Union
 from scipy.stats import nbinom
 import torch.utils.data as td
 from torch import nn
@@ -8,11 +9,17 @@ class Sampler:
     def __init__(self, parameters):
         self.parameters = parameters
 
+    def process_batch(self, l: Union[list, dict]):
+        # ignore training response Y if provided
+        if type(l) is list:
+            _, l = l
+        return l
+
     def sample(loader: td.DataLoader):
         pass
 
 class NegativeBinomialSampler(Sampler):
-    def __init__(self, parameters):
+    def __init__(self, parameters: dict):
         super().__init__(parameters)
 
     def sample(self, loader: td.DataLoader):
@@ -20,22 +27,18 @@ class NegativeBinomialSampler(Sampler):
         f = nn.ModuleDict({k: nn.Linear(n_input[k], n_output, bias=False) for k in n_input.keys()})
 
         samples = []
-        for X in loader:
-            # ignore Y if provided
-            if type(X) is list:
-                _, X = X
-
-            with torch.no_grad():
-                alpha = torch.exp(f["alpha"](X["alpha"]))
-                mu = torch.exp(f["mu"](X["mu"]))
-            total_count = 1 / alpha
-            p = 1 - 1 / (1 + alpha * mu)
-            samples.append(nbinom(n=total_count.numpy(), p=p.numpy()).rvs())
-
+        for l in loader:
+            total_count, p = nb_parameters(f, self.process_batch(l))
+            samples.append(nbinom(n=total_count, p=p).rvs())
         return np.concatenate(samples, axis=0)
 
-def parameter_dims(parameters):
+def parameter_dims(parameters: dict):
     n_output = parameters["mu"].shape[0]
     n_input = {k: v.shape[1] for k, v in parameters.items()}
     return n_output, n_input
     
+def nb_parameters(f: nn.ModuleDict, X: dict):
+    with torch.no_grad():
+        alpha = torch.exp(f["alpha"](X["alpha"])).numpy()
+        mu = torch.exp(f["mu"](X["mu"])).numpy()
+    return 1 / alpha, 1 - 1 / (1 + alpha * mu)
