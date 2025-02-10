7f4529519159ba5f951477867311ac99a7830662
Kris Sankaran <sankaran.kris@gmail.com>
2025-02-10 16:02:02 -0600

    revise nullify

--- scdesigner/src/scdesigner/experimental/transform/mask.py
+++ scdesigner/src/scdesigner/experimental/transform/mask.py
@@ -2,21 +2,30 @@ import numpy as np
 from formulaic import Formula, model_matrix
 import anndata
 
-def str_match(string, string_list):
+
+def str_match(string: str, string_list: list) -> bool:
     for l in string_list:
-        if l.contains(string):
+        if l in string:
             return True
     return False
 
 
-def anndata_formula_mask(outcomes: list, inputs: list, formula: Formula, adata: anndata.AnnData, subset: int=10):
+def anndata_formula_mask(
+    outcomes: list,
+    inputs: list,
+    formula: Formula,
+    adata: anndata.AnnData,
+    subset: int = 10,
+) -> np.array:
     """
-    Mask for Queried Parameters 
+    Mask for Queried Parameters
     """
-    response_index = [adata.var_names[i] in outcomes for i in range(len(adata.var_names))]
-    features = model_matrix(formula, adata.obs.iloc[:subset, :]).columns
-    feature_index = [str_match(features[i], inputs) for i in range(len(features))]
+    response_index = [
+        adata.var_names[i] in outcomes for i in range(len(adata.var_names))
+    ]
+    features = list(model_matrix(formula, adata.obs.iloc[:subset, :]).columns)
+    feature_index = [str_match(f, inputs) for f in features]
 
-    mask = np.zeros((len(adata.var_names), len(features)))
-    mask[response_index, feature_index] = 1
-    return mask
+    mask = np.zeros((len(features), len(adata.var_names)))
+    mask[np.ix_(feature_index, response_index)] = 1
+    return mask.astype(np.int32)
--- scdesigner/src/scdesigner/experimental/transform/nullify.py
+++ scdesigner/src/scdesigner/experimental/transform/nullify.py
@@ -1,7 +1,9 @@
 import numpy as np
 from typing import Union
+from . import docstrings as ds
 
-def nullify(params: dict, id: str, mask: Union[np.array, None]=None) -> dict:
+@ds.doc(ds.nullify)
+def nullify(params: dict, id: str, mask: Union[np.array, None] = None) -> dict:
     if mask is None:
         mask = np.ones(params[id].shape)
 
