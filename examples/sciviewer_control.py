f7852149214d1609b0b4c81b97c3a457e1d0c56e
Kris Sankaran <sankaran.kris@gmail.com>
2025-03-31 18:55:39 -0500

    lord++ implementation for online fdr control

--- examples/sciviewer_control.py
+++ examples/sciviewer_control.py
@@ -1,6 +1,7 @@
 import altair as alt
 import numpy as np
 import pandas as pd
+import scipy.stats as ss
 
 
 def cormat(X, projection):
@@ -90,3 +91,84 @@ def plot_subset(adata, genes, scores):
         .resolve_scale(y="independent")
         .configure_view(continuousWidth=150, continuousHeight=150)
     )
+
+
+def test_direction(X, projection):
+    rs = np.corrcoef(projection, X, rowvar=False)[0, 1:]
+
+    n = len(X)
+    T = -np.abs(rs * np.sqrt(n - 2)) / np.sqrt(1 - (rs**2))
+    return {"correlation": rs, "p_value": ss.t.cdf(T, df=n - 2) * 2}
+
+
+def plot_pval_sequence(online_result, width=600):
+    online_result = online_result.reset_index()
+    online_result["neg_log_pval"] = -np.log(online_result["p_value"])
+
+    return (
+        alt.Chart(online_result)
+        .mark_circle(size=5)
+        .encode(
+            x="index:Q",
+            y="neg_log_pval:Q",
+            color="R:N",
+        )
+        .properties(width=width)
+    )
+
+
+def plot_alpha_sequence(online_result, width=600):
+    online_result = online_result.reset_index()
+
+    return (
+        alt.Chart(online_result)
+        .mark_circle(size=5)
+        .encode(x="index:Q", y="alpha_i:Q")
+        .properties(width=width)
+    )
+
+
+def lord_test(pval, gammai=None, alpha=0.05, w0=0.005):
+    """"
+    This is a direct translation of "version 1" under:
+
+    https://github.com/bioc/onlineFDR/blob/devel/src/lord.cpp
+    """
+    N = len(pval)
+    alphai = np.zeros(N)
+    R = np.zeros(N, dtype=bool)
+    tau = []
+
+    if gammai is None:
+        gammai = (
+            0.07720838
+            * np.log(np.maximum(np.arange(1, N + 2), 2))
+            / (np.arange(1, N + 2) * np.exp(np.sqrt(np.log(np.arange(1, N + 2)))))
+        )
+
+    alphai[0] = gammai[0] * w0
+    R[0] = pval[0] <= alphai[0]
+    if R[0]:
+        tau.append(0)
+    K = int(np.sum(R))
+
+    for i in range(1, N):
+        if K <= 1:
+            if R[i - 1]:
+                tau = [i - 1]
+            Cjsum = sum(gammai[i - tau[j] - 1] for j in range(K))
+            alphai[i] = w0 * gammai[i] + (alpha - w0) * Cjsum
+        else:
+            if R[i - 1]:
+                tau.append(i - 1)
+            tau2 = tau[1:]
+            Cjsum = sum(gammai[i - tau2[j] - 1] for j in range(K - 1))
+            alphai[i] = (
+                w0 * gammai[i] + (alpha - w0) * gammai[i - tau[0] - 1] + alpha * Cjsum
+            )
+
+        if pval[i] <= alphai[i]:
+            R[i] = True
+            K += 1
+
+    return pd.DataFrame({"p_value": pval, "alpha_i": alphai, "R": R})
