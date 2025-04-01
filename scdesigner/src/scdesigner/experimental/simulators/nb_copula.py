21d255dd8209a1cf0d7bff4bda28680ca8d6e13c
Yuliang Peng <peng68@wisc.edu>
2025-04-01 02:00:37 -0500

    reorganized nb regression & copula simulators

--- scdesigner/src/scdesigner/experimental/simulators/nb_copula.py
+++ scdesigner/src/scdesigner/experimental/simulators/nb_copula.py
@@ -1,11 +1,13 @@
 from anndata import AnnData
 from scipy.stats import nbinom, norm
 from formulaic import model_matrix
-from .nb_regression import (
+from ..estimators.glm_regression import (
+    negative_binomial_copula_array,
+    format_input_anndata,
+    format_copula_parameters,
     format_nb_parameters,
-    negative_binomial_regression_array,
+    group_indices,
 )
-import scipy.sparse
 import numpy as np
 import pandas as pd
 
@@ -48,139 +50,41 @@ class NegBinCopulaSimulator:
         x = model_matrix(formula, obs)
         groups = group_indices(formula_copula, obs)
 
-        r, mu, u = negative_binomial_copula_sample_array(parameters, x, groups)
+        G = parameters["coefficient"].shape[1]
+        u = np.zeros((x.shape[0], G))
+
+        # cycle across groups
+        for group, ix in groups.items():
+            if type(parameters["covariance"]) is not dict:
+                parameters["covariance"] = {group: parameters["covariance"]}
+
+            z = np.random.multivariate_normal(
+                mean=np.zeros(G), cov=parameters["covariance"][group], size=len(ix)
+            )
+            normal_distn = norm(0, np.diag(parameters["covariance"][group] ** 0.5))
+            u[ix] = normal_distn.cdf(z)
+
+        r, mu = (
+            self.predict(parameters, obs, formula)["dispersion"],
+            self.predict(parameters, obs, formula)["coefficient"],
+        )
         samples = nbinom(n=r, p=r / (r + mu)).ppf(u)
         result = AnnData(X=samples, obs=obs)
         result.var_names = parameters["dispersion"].columns
         return result
 
-    def predict(
-        self, parameters: dict, obs: pd.DataFrame, formula="~ 1", formula_copula="~ 1"
-    ) -> dict:
+    def predict(self, parameters: dict, obs: pd.DataFrame, formula="~ 1") -> dict:
         x = model_matrix(formula, obs)
-        groups = group_indices(formula_copula, obs)
-        r, mu, _ = negative_binomial_copula_sample_array(parameters, x, groups)
+        r, mu = np.exp(parameters["dispersion"]), np.exp(x @ parameters["coefficient"])
+        r = np.repeat(r, mu.shape[0], axis=0)
         return {
             "coefficient": mu,
             "dispersion": r,
-            "covariance": parameters["covariance"],
         }
 
-    def __str__(self):
-        return f"""scDesigner object with n_obs x n_vars = {self.shape[0]} x {self.shape[1]}
-    method: 'NBCopula'
+    def __repr__(self):
+        return f"""scDesigner simulator object with
+    method: 'Negtive Binomial Copula'
     formula: '{self.formula}'
     copula formula: '{self.copula_formula}'
     parameters: 'coefficient', 'dispersion', 'covariance'"""
-
-
-def negative_binomial_copula_array(
-    x: np.array,
-    y: np.array,
-    groups: dict,
-    batch_size: int = 512,
-    lr: float = 0.1,
-    epochs: int = 20,
-) -> dict:
-    """
-    A minimal NB copula model
-
-    # simulate data
-    n_samples, n_features, n_outcomes = 1000, 2, 4
-    x_sim = np.random.normal(size=(n_samples, n_features))
-    beta_sim = np.random.normal(size=(n_features, n_outcomes))
-    mu_sim = np.exp(x_sim @ beta_sim)
-    r_sim = np.random.uniform(.5, 1.5, n_outcomes)
-    y_sim = np.random.negative_binomial(r_sim, r_sim / (r_sim + mu_sim))
-    y_sim[:, 1] = y_sim[:, 0]
-
-    # estimate model
-    negative_binomial_copula(x_sim, y_sim)
-    """
-    # get predicted mean and dispersions
-    parameters = negative_binomial_regression_array(x, y, batch_size, lr, epochs)
-    r, mu = np.exp(parameters["dispersion"]), np.exp(x @ parameters["coefficient"])
-    nb_distn = nbinom(n=r, p=r / (r + mu))
-
-    # gaussianize and estimate covariance
-    alpha = np.random.uniform(size=y.shape)
-    u = clip(alpha * nb_distn.cdf(y) + (1 - alpha) * nb_distn.cdf(1 + y))
-    parameters["covariance"] = copula_covariance(u, groups)
-    return parameters
-
-
-def negative_binomial_copula_sample_array(
-    parameters: dict, x: np.array, groups: dict
-) -> np.array:
-    # initialize uniformized gaussian samples
-    G = parameters["coefficient"].shape[1]
-    u = np.zeros((x.shape[0], G))
-
-    # cycle across groups
-    for group, ix in groups.items():
-        if type(parameters["covariance"]) is not dict:
-            parameters["covariance"] = {group: parameters["covariance"]}
-
-        z = np.random.multivariate_normal(
-            mean=np.zeros(G), cov=parameters["covariance"][group], size=len(ix)
-        )
-        normal_distn = norm(0, np.diag(parameters["covariance"][group] ** 0.5))
-        u[ix] = normal_distn.cdf(z)
-
-    # invert using negative binomial margins
-    r, mu = np.exp(parameters["dispersion"]), np.exp(x @ parameters["coefficient"])
-    r = np.repeat(r, mu.shape[0], axis=0)
-    return r, mu, u
-
-
-###############################################################################
-## Helpers for fitting & sampling NB Copula
-###############################################################################
-
-
-def copula_covariance(u: np.array, groups: dict):
-    result = {}
-    for group, ix in groups.items():
-        result[group] = np.cov(norm().ppf(u[ix]).T)
-
-    if len(result) == 1:
-        return list(result.values())[0]
-    return result
-
-
-def clip(u: np.array, min: float = 1e-5, max: float = 1 - 1e-5) -> np.array:
-    u[u < min] = min
-    u[u > max] = max
-    return u
-
-
-def format_input_anndata(adata: AnnData) -> AnnData:
-    result = adata.copy()
-    if isinstance(result.X, scipy.sparse._csc.csc_matrix):
-        result.X = result.X.todense()
-    return result
-
-
-def format_copula_parameters(parameters: dict, var_names: list):
-    covariance = parameters["covariance"]
-    if type(covariance) is not dict:
-        covariance = pd.DataFrame(
-            parameters["covariance"], columns=list(var_names), index=list(var_names)
-        )
-    else:
-        for group in covariance.keys():
-            covariance[group] = pd.DataFrame(
-                parameters["covariance"][group],
-                columns=list(var_names),
-                index=list(var_names),
-            )
-    return covariance
-
-
-def group_indices(formula: str, obs: pd.DataFrame) -> dict:
-    group_matrix = model_matrix(formula, obs)
-    result = {}
-
-    for group in group_matrix.columns:
-        result[group] = np.where(group_matrix[group].values == 1)[0]
-    return result
--- scdesigner/src/scdesigner/experimental/simulators/nb_regression.py
+++ scdesigner/src/scdesigner/experimental/simulators/nb_regression.py
@@ -1,12 +1,14 @@
 from anndata import AnnData
-from copy import deepcopy
 from formulaic import model_matrix
 from scipy.stats import nbinom
 import scipy.sparse
-from torch.utils.data import DataLoader, TensorDataset
+from ..estimators.glm_regression import (
+    negative_binomial_regression_array,
+    format_input_anndata,
+    format_nb_parameters,
+)
 import numpy as np
 import pandas as pd
-import torch
 
 
 class NegBinRegressionSimulator:
@@ -29,7 +31,8 @@ class NegBinRegressionSimulator:
         else:
             x = obs
 
-        r, mu = negative_binomial_regression_sample_array(parameters, x)
+        params = self.predict(parameters, x)
+        r, mu = params["dispersion"], params["coefficient"]
         samples = nbinom(n=r, p=r / (r + mu)).rvs()
         result = AnnData(X=samples, obs=obs)
         result.var_names = parameters["dispersion"].columns
@@ -41,118 +44,12 @@ class NegBinRegressionSimulator:
         else:
             x = obs
 
-        r, mu = negative_binomial_regression_sample_array(parameters, x)
+        r, mu = np.exp(parameters["dispersion"]), np.exp(x @ parameters["coefficient"])
+        r = np.repeat(r, mu.shape[0], axis=0)
         return {"coefficient": mu, "dispersion": r}
 
-    def __str__(self):
-        return f"""scDesigner object with n_obs x n_vars = {self.shape[0]} x {self.shape[1]}
-    method: 'NBRegression'
+    def __repr__(self):
+        return f"""scDesigner simulator object with
+    method: 'Negtive Binomial Regression'
     formula: '{self.formula}'
     parameters: 'coefficient', 'dispersion'"""
-
-
-def negative_binomial_regression_array(
-    x: np.array, y: np.array, batch_size: int = 512, lr: float = 0.1, epochs: int = 40
-) -> dict:
-    """
-    A minimal NB regression model
-
-    # simulate data
-    n_samples, n_features, n_outcomes = 1000, 2, 4
-    x_sim = np.random.normal(size=(n_samples, n_features))
-    beta_sim = np.random.normal(size=(n_features, n_outcomes))
-    mu_sim = np.exp(x_sim @ beta_sim)
-    r_sim = np.random.uniform(.5, 1.5, n_outcomes)
-    y_sim = np.random.negative_binomial(r_sim, r_sim / (r_sim + mu_sim))
-
-    # estimate model
-    negative_binomial_regression(x_sim, y_sim)
-    """
-
-    device = check_device()
-    dataset = TensorDataset(
-        torch.tensor(x, dtype=torch.float32).to(device),
-        torch.tensor(y, dtype=torch.float32).to(device),
-    )
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-
-    n_features, n_outcomes = x.shape[1], y.shape[1]
-    params = torch.zeros(
-        n_features * n_outcomes + n_outcomes, requires_grad=True, device=device
-    )
-    optimizer = torch.optim.Adam([params], lr=lr)
-
-    for _ in range(epochs):
-        for x_batch, y_batch in dataloader:
-            optimizer.zero_grad()
-            loss = negative_binomial_regression_likelihood(params, x_batch, y_batch)
-            loss.backward()
-            optimizer.step()
-
-    b_elem = n_features * n_outcomes
-    beta = to_np(params[:b_elem]).reshape(n_features, n_outcomes)
-    dispersion = np.exp(to_np(params[b_elem:]))
-    return {"coefficient": beta, "dispersion": dispersion}
-
-
-def negative_binomial_regression_sample_array(
-    parameters: dict, x: np.array
-) -> np.array:
-    r, mu = np.exp(parameters["dispersion"]), np.exp(x @ parameters["coefficient"])
-    r = np.repeat(r, mu.shape[0], axis=0)
-    return r, mu
-
-
-###############################################################################
-## Helpers for fitting & sampling NB regression
-###############################################################################
-
-
-def negative_binomial_regression_likelihood(params, X, y):
-    n_features = X.shape[1]
-    n_outcomes = y.shape[1]
-
-    # form the mean and dispersion parameters
-    beta = params[: n_features * n_outcomes].reshape(n_features, n_outcomes)
-    log_r = params[n_features * n_outcomes :]
-    r, mu = torch.exp(log_r), torch.exp(X @ beta)
-
-    # compute the negative log likelihood
-    log_likelihood = (
-        torch.lgamma(y + r)
-        - torch.lgamma(r)
-        - torch.lgamma(y + 1)
-        + r * torch.log(r)
-        + y * torch.log(mu)
-        - (r + y) * torch.log(r + mu)
-    )
-    return -torch.sum(log_likelihood)
-
-
-def format_input_anndata(adata: AnnData) -> AnnData:
-    result = adata.copy()
-    if isinstance(result.X, scipy.sparse._csc.csc_matrix):
-        result.X = result.X.todense()
-    return result
-
-
-def format_nb_parameters(parameters: dict, var_names: list, coef_index: list) -> dict:
-    parameters["coefficient"] = pd.DataFrame(
-        parameters["coefficient"], columns=var_names, index=coef_index
-    )
-    parameters["dispersion"] = pd.DataFrame(
-        parameters["dispersion"].reshape(1, -1), columns=var_names, index=["dispersion"]
-    )
-    return parameters
-
-
-def check_device():
-    return torch.device(
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps" if torch.backends.mps.is_available() else "cpu"
-    )
-
-
-def to_np(x):
-    return x.detach().cpu().numpy()
