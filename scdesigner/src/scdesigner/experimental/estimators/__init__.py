63a67f13e83fb94d27eb5ab97ef6155783330e0a
Kris Sankaran <sankaran.kris@gmail.com>
2025-04-03 18:51:05 -0500

    copula factory functions

--- scdesigner/src/scdesigner/experimental/estimators/__init__.py
+++ scdesigner/src/scdesigner/experimental/estimators/__init__.py
@@ -3,7 +3,7 @@ from .mixed_effects import LinearMixedEffectsEstimator, PoissonMixedEffectsEstim
 from .estimators import GeneralizedLinearModelML
 from .glm_regression import negative_binomial_regression, negative_binomial_copula
 from .glm_regression_ondisk import AdataViewDataset, negative_binomial_regression_array_ondisk
-from .negbin import negbin_regression2
+from .negbin import negbin_regression2, negbin_copula
 from .poisson import poisson_regression
 from .bernoulli import bernoulli_regression
 from .zero_inflated_negbin import zero_inflated_negbin_regression
@@ -20,5 +20,6 @@ __all__ = [
     "poisson_regression",
     "negative_binomial_regression_array_ondisk",
     "zero_inflated_negbin_regression",
+    "negbin_copula",
     "bernoulli_regression"
 ]
