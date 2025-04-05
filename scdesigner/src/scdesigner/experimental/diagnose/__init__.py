f74bef95f4b160fb448377cf05ea3d98c164e2bf
Kris Sankaran <sankaran.kris@gmail.com>
2025-04-05 11:12:12 -0500

    analogous function for PCA comparison plot

--- scdesigner/src/scdesigner/experimental/diagnose/__init__.py
+++ scdesigner/src/scdesigner/experimental/diagnose/__init__.py
@@ -4,12 +4,15 @@ from .plot import (
     compare_variance,
     compare_standard_deviation,
     compare_umap,
+    compare_pca,
 )
 
 __all__ = [
     "plot_umap",
+    "plot_pca",
     "compare_means",
     "compare_variance",
     "compare_standard_deviation",
     "compare_umap",
+    "compare_pca",
 ]
--- scdesigner/src/scdesigner/experimental/diagnose/plot.py
+++ scdesigner/src/scdesigner/experimental/diagnose/plot.py
@@ -12,7 +12,7 @@ def plot_umap(
     opacity=0.6,
     n_comps=20,
     n_neighbors=15,
-    transform = lambda x: np.log1p(x),
+    transform=lambda x: np.log1p(x),
     **kwargs
 ):
     mapping = {"x": "UMAP1", "y": "UMAP2", "color": color, "shape": shape}
@@ -33,6 +33,35 @@ def plot_umap(
 
     # encode and visualize
     chart = alt.Chart(umap_df).mark_point(opacity=opacity).encode(**mapping)
+    if facet is not None:
+        chart = chart.facet(column=alt.Facet(facet))
+    return chart
+
+
+def plot_pca(
+    adata,
+    color=None,
+    shape=None,
+    facet=None,
+    opacity=0.6,
+    plot_dims=[0, 1],
+    transform=lambda x: np.log1p(x),
+    **kwargs
+):
+    mapping = {"x": "PCA1", "y": "PCA2", "color": color, "shape": shape}
+    mapping = {k: v for k, v in mapping.items() if v is not None}
+
+    adata_ = adata.copy()
+    adata_.X = check_sparse(adata_.X)
+    adata_.X = transform(adata_.X)
+
+    # get PCA scores
+    sc.pp.pca(adata_, **kwargs)
+    pca_df = pd.DataFrame(adata_.obsm["X_pca"][:, plot_dims], columns=["PCA1", "PCA2"])
+    pca_df = pd.concat([pca_df, adata_.obs.reset_index(drop=True)], axis=1)
+
+    # plot
+    chart = alt.Chart(pca_df).mark_point(opacity=opacity).encode(**mapping)
     if facet is not None:
         chart = chart.facet(column=alt.Facet(facet))
     return chart
@@ -63,6 +92,7 @@ def compare_means(real, simulated, transform=lambda x: x):
     summary = lambda a: np.asarray(transform(a.X).mean(axis=0)).flatten()
     return compare_summary(real_, simulated_, summary)
 
+
 def prepare_dense(real, simulated):
     real_ = real.copy()
     simulated_ = simulated.copy()
@@ -83,9 +113,18 @@ def compare_standard_deviation(real, simulated, transform=lambda x: x):
     return compare_summary(real_, simulated_, summary)
 
 
-def compare_umap(real, simulated, transform = lambda x: x, **kwargs):
+def concat_real_sim(real, simulated):
     real_, simulated_ = prepare_dense(real, simulated)
     real_.obs["source"] = "real"
     simulated_.obs["source"] = "simulated"
-    adata_merged = real_.concatenate(simulated_, join="outer", batch_key=None)
-    return plot_umap(adata_merged, facet="source", transform=transform, **kwargs)
+    return real_.concatenate(simulated_, join="outer", batch_key=None)
+
+
+def compare_umap(real, simulated, transform=lambda x: x, **kwargs):
+    adata = concat_real_sim(real, simulated)
+    return plot_umap(adata, facet="source", transform=transform, **kwargs)
+
+
+def compare_pca(real, simulated, transform=lambda x: x, **kwargs):
+    adata = concat_real_sim(real, simulated)
+    return plot_pca(adata, facet="source", transform=transform, **kwargs)
