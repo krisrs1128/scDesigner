48d4730cfbde7481dc46eef7b72f795cf8e11e6f
Kris Sankaran <sankaran.kris@gmail.com>
2025-04-05 11:05:48 -0500

    export umap comparison

--- scdesigner/src/scdesigner/experimental/diagnose/__init__.py
+++ scdesigner/src/scdesigner/experimental/diagnose/__init__.py
@@ -1,8 +1,15 @@
-from .plot import plot_umap, compare_means, compare_variance, compare_standard_deviation
+from .plot import (
+    plot_umap,
+    compare_means,
+    compare_variance,
+    compare_standard_deviation,
+    compare_umap,
+)
 
 __all__ = [
     "plot_umap",
     "compare_means",
     "compare_variance",
-    "compare_standard_deviation"
+    "compare_standard_deviation",
+    "compare_umap",
 ]
