ee402eae466575804db088ee0a26470568fbe5b0
Kris Sankaran <sankaran.kris@gmail.com>
2025-09-08 19:41:00 -0500

    fix parameter lookup

--- scdesigner/src/scdesigner/predictors/bernoulli.py
+++ scdesigner/src/scdesigner/predictors/bernoulli.py
@@ -5,5 +5,5 @@ from ..format import format_matrix
 
 def bernoulli_predict(parameters: dict, obs: pd.DataFrame, formula: str):
     x = format_matrix(obs, formula)
-    theta = np.exp(x @ parameters["beta"])
+    theta = np.exp(x @ parameters["coef_mean"])
     return {"mean": theta}
--- scdesigner/src/scdesigner/samplers/__init__.py
+++ scdesigner/src/scdesigner/samplers/__init__.py
@@ -1,4 +1,3 @@
-from .mixed_effects import LinearMixedEffectsSampler, PoissonMixedEffectsSampler
 from .negbin import negbin_sample, negbin_copula_sample
 from .poisson import poisson_sample, poisson_copula_sample
 from .bernoulli import bernoulli_sample, bernoulli_copula_sample
@@ -9,14 +8,13 @@ from .zero_inflated_negbin import (
 from .zero_inflated_poisson import zero_inflated_poisson_sample
 
 __all__ = [
-    "LinearMixedEffectsSampler",
-    "PoissonMixedEffectsSampler",
     "negbin_sample",
     "negbin_copula_sample",
     "poisson_sample",
     "poisson_copula_sample",
     "bernoulli_sample",
-    "bernoulli_copula_sample" "zero_inflated_negbin_sample",
+    "bernoulli_copula_sample",
+    "zero_inflated_negbin_sample",
     "zero_inflated_negbin_copula_sample",
     "zero_inflated_poisson_sample",
 ]
