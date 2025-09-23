f3a14683646ec27b4f5b42b76dc78dcb44f2805d
Kris Sankaran <sankaran.kris@gmail.com>
2025-09-22 22:06:25 -0500

    minimal example using marginal + copula classes

---
+++ scdesigner/src/scdesigner/minimal/copula.py
@@ -0,0 +1,29 @@
+from typing import Dict, Callable
+from anndata import AnnData
+from loader import adata_loader
+
+class Copula:
+    def __init__(self, formula: str, **kwargs):
+        self.formula = formula
+        self.loader = None
+        self.n_outcomes = None
+        self.parameters = None
+
+    def setup_data(self, adata: AnnData, marginal_formula: Dict[str, str], batch_size: int = 32, **kwargs):
+        self.adata = adata
+        self.formula = self.formula | marginal_formula
+        self.loader = adata_loader(adata, self.formula, batch_size=batch_size, **kwargs)
+        X_batch, _ = next(iter(self.loader))
+        self.n_outcomes = X_batch.shape[1]
+
+    def fit(self, uniformizer: Callable, **kwargs):
+        raise NotImplementedError
+
+    def pseudo_obs(self, x_dict: Dict):
+        raise NotImplementedError
+
+    def num_params(self, **kwargs):
+        raise NotImplementedError
+
+    def format_parameters(self):
+        raise NotImplementedError
---
+++ scdesigner/src/scdesigner/minimal/standard_covariance.py
@@ -0,0 +1,102 @@
+from copula import Copula
+from formula import standardize_formula
+from anndata import AnnData
+from typing import Dict, Union
+import torch
+from tqdm import tqdm
+import numpy as np
+from scipy.stats import norm
+import pandas as pd
+
+
+class StandardCovariance(Copula):
+    def __init__(self, formula: str = "~ 1"):
+        formula = standardize_formula(formula, allowed_keys=['group'])
+        super().__init__(formula)
+        self.groups = None
+
+
+    def setup_data(self, adata: AnnData, marginal_formula: Dict[str, str], batch_size: int = 32, **kwargs):
+        super().setup_data(adata, marginal_formula, batch_size, **kwargs)
+        _, obs_batch = next(iter(self.loader))
+        obs_batch_group = obs_batch.get("group")
+
+        # fill in group indexing variables
+        self.groups = self.loader.dataset.predictor_names["group"]
+        self.n_groups = len(self.groups)
+        self.group_col = {g: i for i, g in enumerate(self.groups)}
+
+        # check that obs_batch is a binary grouping matrix
+        unique_vals = torch.unique(obs_batch_group)
+        if (not torch.all((unique_vals == 0) | (unique_vals == 1)).item()):
+            raise ValueError("Only categorical groups are currently supported in copula covariance estimation.")
+
+    def fit(self, uniformizer, **kwargs):
+        sums = {g: np.zeros(self.n_outcomes) for g in self.groups}
+        second_moments = {g: np.eye(self.n_outcomes) for g in self.groups}
+        Ng = {g: 0 for g in self.groups}
+        
+        for y, x_dict in tqdm(self.loader, desc="Estimating copula covariance"):
+            memberships = x_dict.get("group").numpy()
+            u = uniformizer(y, x_dict)
+
+            for g in self.groups:
+                ix = np.where(memberships[:, self.group_col[g]] == 1)
+                z = norm().ppf(u[ix])
+                second_moments[g] += z.T @ z
+                sums[g] += z.sum(axis=0)
+                Ng[g] += len(ix[0])
+
+        covariances = {}
+        for g in self.groups:
+            mean = sums[g] / Ng[g]
+            covariances[g] = second_moments[g] / Ng[g] - np.outer(mean, mean)
+
+        if len(self.groups) == 1:
+            covariances = covariances.values()[0] 
+        self.parameters = self.format_parameters(covariances)
+
+    def num_params(self, **kwargs):
+        pass
+
+    def format_parameters(self, covariances: Union[Dict, np.array]):
+        var_names = self.adata.var_names
+        def to_df(mat):
+            return pd.DataFrame(mat, index=var_names, columns=var_names)
+
+        if isinstance(covariances, dict):
+            formatted = {}
+            for k, v in covariances.items():
+                formatted[k] = to_df(v)
+            covariances = formatted
+            return covariances
+
+        if isinstance(covariances, (np.ndarray, list, tuple)):
+            covariances = to_df(covariances)
+        return covariances
+
+    def pseudo_obs(self, x_dict: Dict):
+        # convert one-hot encoding memberships to a map
+        #      {"group1": [indices of group 1], "group2": [indices of group 2]}
+        memberships = x_dict.get("group").numpy()
+        group_ix = {g: np.where(memberships[:, self.group_col[g] == 1])[0] for g in self.groups}
+
+        # initialize the result
+        u = np.zeros((len(memberships), self.n_outcomes))
+        if type(self.parameters) is not dict:
+            self.parameters = {group: self.parameters}
+
+        # loop over groups and sample each part in turn
+        for group, sigma in self.parameters.items():
+            z = np.random.multivariate_normal(
+                mean=np.zeros(self.n_outcomes), 
+                cov=sigma, 
+                size=len(group_ix[group])
+            )
+            normal_distn = norm(0, np.diag(sigma) ** 0.5)
+            u[group_ix[group]] = normal_distn.cdf(z)
+        return u
+
+    def num_params(self, **kwargs):
+        S = self.parameters
+        return {g: (np.sum(S[g] != 0) - S[g].shape[0]) / 2 for g in self.groups}
