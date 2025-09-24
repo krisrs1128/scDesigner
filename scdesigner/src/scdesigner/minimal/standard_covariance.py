473499e046e1f94ab8ac250b730a0136f291a437
Kris Sankaran <sankaran.kris@gmail.com>
2025-09-23 20:58:14 -0500

    function to compute likelihood in copula instantiation

--- scdesigner/src/scdesigner/minimal/standard_covariance.py
+++ scdesigner/src/scdesigner/minimal/standard_covariance.py
@@ -2,9 +2,9 @@ from .copula import Copula
 from .formula import standardize_formula
 from .kwargs import DEFAULT_ALLOWED_KWARGS, _filter_kwargs
 from anndata import AnnData
-from scipy.stats import norm
+from scipy.stats import norm, multivariate_normal
 from tqdm import tqdm
-from typing import Dict, Union
+from typing import Dict, Union, Callable, Tuple
 import numpy as np
 import pandas as pd
 import torch
@@ -17,9 +17,8 @@ class StandardCovariance(Copula):
         self.groups = None
 
 
-    def setup_data(self, adata: AnnData, marginal_formula: Dict[str, str], batch_size: int = 32, **kwargs):
+    def setup_data(self, adata: AnnData, marginal_formula: Dict[str, str], **kwargs):
         data_kwargs = _filter_kwargs(kwargs, DEFAULT_ALLOWED_KWARGS['data'])
-        data_kwargs["batch_size"] = batch_size
         super().setup_data(adata, marginal_formula, **data_kwargs)
         _, obs_batch = next(iter(self.loader))
         obs_batch_group = obs_batch.get("group")
@@ -34,7 +33,7 @@ class StandardCovariance(Copula):
         if (not torch.all((unique_vals == 0) | (unique_vals == 1)).item()):
             raise ValueError("Only categorical groups are currently supported in copula covariance estimation.")
 
-    def fit(self, uniformizer, **kwargs):
+    def fit(self, uniformizer: Callable, **kwargs):
         sums = {g: np.zeros(self.n_outcomes) for g in self.groups}
         second_moments = {g: np.eye(self.n_outcomes) for g in self.groups}
         Ng = {g: 0 for g in self.groups}
@@ -58,9 +57,6 @@ class StandardCovariance(Copula):
         if len(self.groups) == 1:
             covariances = covariances.values()[0]
         self.parameters = self.format_parameters(covariances)
-
-    def num_params(self, **kwargs):
-        pass
 
     def format_parameters(self, covariances: Union[Dict, np.array]):
         var_names = self.adata.var_names
@@ -86,11 +82,12 @@ class StandardCovariance(Copula):
 
         # initialize the result
         u = np.zeros((len(memberships), self.n_outcomes))
-        if type(self.parameters) is not dict:
-            self.parameters = {group: self.parameters}
+        parameters = self.parameters
+        if type(parameters) is not dict:
+            parameters = {group: parameters}
 
         # loop over groups and sample each part in turn
-        for group, sigma in self.parameters.items():
+        for group, sigma in parameters.items():
             z = np.random.multivariate_normal(
                 mean=np.zeros(self.n_outcomes),
                 cov=sigma,
@@ -100,6 +97,28 @@ class StandardCovariance(Copula):
             u[group_ix[group]] = normal_distn.cdf(z)
         return u
 
+    def likelihood(self, uniformizer: Callable, batch: Tuple[torch.Tensor, Dict[str, torch.Tensor]]):
+        # uniformize the observations
+        y, x_dict = batch
+        u = uniformizer(y, x_dict)
+        z = norm().ppf(u)
+
+        # same group manipulation as for pseudobs
+        parameters = self.parameters
+        if type(parameters) is not dict:
+            parameters = {group: parameters}
+
+        memberships = x_dict.get("group").numpy()
+        group_ix = {g: np.where(memberships[:, self.group_col[g] == 1])[0] for g in self.groups}
+        ll = np.zeros(len(z))
+        for group, sigma in parameters.items():
+            ix = group_ix[group]
+            if len(ix) > 0:
+                copula_ll = multivariate_normal.logpdf(z[ix], np.zeros(sigma.shape[0]), sigma)
+                ll[ix] = copula_ll - norm.logpdf(z[ix]).sum(axis=1)
+        return ll
+
     def num_params(self, **kwargs):
         S = self.parameters
-        return {g: (np.sum(S[g] != 0) - S[g].shape[0]) / 2 for g in self.groups}
+        per_group = [(np.sum(S[g].values != 0) - S[g].shape[0]) / 2 for g in self.groups]
+        return sum(per_group)
