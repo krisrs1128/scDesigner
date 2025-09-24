10c3bd2cf92a33639a20a588291834a2553840b7
Kris Sankaran <sankaran.kris@gmail.com>
2025-09-23 19:25:18 -0500

    basic simulator wrapper, but not allowing hyperparameters

---
+++ scdesigner/src/scdesigner/minimal/simulator.py
@@ -0,0 +1,75 @@
+from .copula import Copula
+from .marginal import Marginal
+from .loader import obs_loader
+from anndata import AnnData
+import torch
+
+
+class scdesigner:
+    """Simulation wrapper"""
+
+    def __init__(self, marginal: Marginal, copula: Copula):
+        self.marginal = marginal
+        self.copula = copula
+        self.template = None
+        self.parameters = None
+
+    def fit(self, adata: AnnData, **kwargs):
+        """Fit the simulator"""
+        self.template = adata
+
+        self.marginal.setup_data(adata, batch_size=int(256))
+        self.marginal.setup_optimizer(lr=0.01)
+        self.marginal.fit(max_epochs=1)
+
+        self.copula.setup_data(adata, self.marginal.formula)
+        self.copula.fit(self.marginal.uniformize)
+        self.parameters = {
+            "marginal": self.marginal.parameters,
+            "copula": self.copula.parameters
+        }
+
+    def predict(self, obs=None, batch_size: int = 1000, **kwargs):
+        """Predict from an obs dataframe"""
+        # prepare an internal data loader for this obs
+        if obs is None:
+            obs = self.template.obs
+        loader = obs_loader(
+            obs,
+            self.marginal.formula,
+            batch_size=batch_size,
+            **kwargs
+        )
+
+        # get predictions across batches
+        local_parameters = []
+        for _, x_dict in loader:
+            l = self.marginal.predict(x_dict)
+            local_parameters.append(l)
+
+        # convert to a merged dictionary
+        keys = list(local_parameters[0].keys())
+        return {
+            k: torch.cat([d[k] for d in local_parameters]).detach().cpu().numpy()
+            for k in keys
+        }
+
+    def sample(self, obs=None, batch_size: int = 1000, **kwargs):
+        """Generate samples."""
+        if obs is None:
+            obs = self.template.obs
+        loader = obs_loader(
+            obs,
+            self.copula.formula | self.marginal.formula,
+            batch_size=batch_size,
+            **kwargs
+        )
+
+        # get samples across batches
+        samples = []
+        for _, x_dict in loader:
+            u = self.copula.pseudo_obs(x_dict)
+            u = torch.from_numpy(u)
+            samples.append(self.marginal.invert(u, x_dict))
+        samples = torch.cat(samples).detach().cpu().numpy()
+        return AnnData(X = samples, obs=obs)
