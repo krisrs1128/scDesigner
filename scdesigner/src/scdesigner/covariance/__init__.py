23b9aaf14be960dd1d0a6d3f07cbc23f713ca098
Kris Sankaran <sankaran.kris@gmail.com>
2026-08-05 08:04:16 -0400

    split imports for copula vs. general simulation

---
+++ scdesigner/src/scdesigner/covariance/__init__.py
@@ -0,0 +1,25 @@
+"""Covariance estimators for copula dependence structures."""
+
+from .base import CovarianceEstimator, as_covariance_estimator, register_estimator
+from .moments import SampleCovariance
+from .shrinkage import (
+    FixedShrinkage,
+    LedoitWolf,
+    OAS,
+    ledoit_wolf_intensity,
+    oas_intensity,
+    shrink_covariance,
+)
+
+__all__ = [
+    "as_covariance_estimator",
+    "CovarianceEstimator",
+    "FixedShrinkage",
+    "LedoitWolf",
+    "ledoit_wolf_intensity",
+    "OAS",
+    "oas_intensity",
+    "register_estimator",
+    "SampleCovariance",
+    "shrink_covariance",
+]
